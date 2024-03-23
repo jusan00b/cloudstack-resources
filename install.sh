2773934f128b821526d937126621848e13244465
jusan00b <sravansiddarthamudili@gmail.com>
2024-03-23 20:02:15 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -18,19 +18,8 @@ read interface
 ip=$(ip a | grep $interface | grep inet | awk '{print $2}' | cut -d '/' -f1)
 gateway=$(ip route | grep default | awk '{print $3}')
 
-#Update the /etc/hosts file
-sed -i '2a\'"$ip apache.$hostname.u1 $hostname" /etc/hosts
-sudo hostnamectl set-hostname apache.$hostname.u1
-
-echo "Hostname is set to: "
-hostname --fqdn
-
 #Install bridge-utils
 sudo apt install bridge-utils -y
-
-#Create a bridge br0
-sudo brctl addbr br0
-sudo brctl addif br0 $interface
 
 #Configure the ethernet and bridge in network manager
 netowrk_manager="/etc/netplan/01-network-manager-all.yaml"
@@ -39,23 +28,37 @@ network:
   version: 2
   renderer: networkd
   ethernets:
-    $interface:
-      dhcp4: no
-      dhcp6: no
-  bridges:
-    br0:
-      interfaces: [$interface]
-      dhcp4: no
-      dhcp6: no
-      addresses: [$ip/24]
-      gateway4: $gateway
-      nameservers:
-	addresses: [8.8.8.8, 8.8.4.4]
+     $interface:
+	 dhcp4: false
+	 dhcp6: false
+	 optional: true
+     bridges:
+      br0:
+        addresses: [$ip/24]
+	routes:
+	  - to: default
+	    via: $gateway
+	nameservers:
+          addresses: [8.8.8.8,8.8.4.4]
+	interfaces: [$interface]
+	dhcp4: false
+	dhcp6: false
+	parameters:
+	  stp: false
+	  forward-delay: 0
 EOM
 
 #Apply the changes
-sudo netplan apply
-sudo systemctl restart NetworkManager
+sudo netplan generate
+sudo netplan --debbug apply
+sudo systemctl restart NetworkManager 
+
+#Update the /etc/hosts file
+sed -i '2a\'"$ip apache.$hostname.u1 $hostname" /etc/hosts
+sudo hostnamectl set-hostname apache.$hostname.u1
+
+echo "Hostname is set to: "
+hostname --fqdn
 
 #Install the required packages
 sudo apt install ntp -y
@@ -66,7 +69,6 @@ sudo apt install openjdk-11-jdk -y
 echo "deb https://download.cloudstack.org/ubuntu jammy 4.18" | sudo tee /etc/apt/sources.list.d/cloudstack.list
 
 wget -O - https://download.cloudstack.org/release.asc | sudo tee /etc/apt/trusted.gpg.d/cloudstack.asc
-sudo apt update
 
 #Install the cloudstack management server
 sudo apt install cloudstack-management cloudstack-usage -y
@@ -124,7 +126,7 @@ sudo mkdir -p /export/secondary
 
 echo "/export *(rw,async,no_root_squash,no_subtree_check)" >> /etc/exports
 sudo apt install nfs-kernel-server -y
-sudo systemctl restart nfs-kernel-server
+service nfs-kernel-server restart
 
 sudo mkdir -p /mnt/primary
 sudo mkdir -p /mnt/secondary
@@ -132,6 +134,9 @@ sudo mkdir -p /mnt/secondary
 #Mount the NFS shares
 sudo mount -t nfs $hostname:/export/primary /mnt/primary
 sudo mount -t nfs $hostname:/export/secondary /mnt/secondary
+
+#Start the cloudstack management server
+service cloudstack-management start
 
 #Virtualization dependencies installation
 sudo apt install openntpd openssh-server sudo neovim htop tar net-tools -y
@@ -149,7 +154,7 @@ echo 'listen_tcp = 1' >> /etc/libvirt/libvirtd.conf
 echo 'tcp_port = "16509"' >> /etc/libvirt/libvirtd.conf
 echo 'mdns_adv = 0' >> /etc/libvirt/libvirtd.conf
 echo 'auth_tcp = "none"' >> /etc/libvirt/libvirtd.conf
-systemctl restart libvirtd
+sudo systemctl restart libvirtd
 
 #Disable the apparmor for libvirtd
 ln -s /etc/apparmor.d/usr.sbin.libvirtd /etc/apparmor.d/disable/
@@ -159,6 +164,9 @@ apparmor_parser -R /etc/apparmor.d/usr.lib.libvirt.virt-aa-helper
 
 #Check kvm status
 lsmod | grep kvm
+echo "Output should be like this: "
 echo "kvm_intel 55496 0"
 echo "kvm 337772 1 kvm_intel"
 echo "kvm_amd # if you are in AMD cpu"
+
+firefox https://$ip:8080/client
